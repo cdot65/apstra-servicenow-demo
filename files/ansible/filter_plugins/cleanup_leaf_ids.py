1470a3900ad20ebc6ad1ec05529e3ef1df0f8475
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-04-24 11:42:45 +0000

    reteive leaf ids

---
+++ files/ansible/filter_plugins/cleanup_leaf_ids.py
@@ -0,0 +1,38 @@
+"""Filter plugin to cleanup blueprint health response."""
+
+
+class FilterModule(object):
+    """Cleaning up blueprint health response."""
+
+    def filters(self):
+        """Add our filter plugin to the list of built-in plugins."""
+        return {
+            "cleanup_leaf_ids": self.cleanup_leaf_ids,
+        }
+
+    def cleanup_leaf_ids(self, leaf_system_ids):
+        """Filter out filler from the response of our blueprint health."""
+        leafs = {}
+
+        if isinstance(leaf_system_ids["items"], list):
+            for each in leaf_system_ids["items"]:
+                if each["leaf"]["label"] == "leaf1":
+                    leafs["leaf1"] = {}
+                    leafs["leaf1"]["hostname"] = each["leaf"]["hostname"]
+                    leafs["leaf1"]["id"] = each["leaf"]["id"]
+                    leafs["leaf1"]["system_id"] = each["leaf"]["system_id"]
+                    leafs["leaf1"]["system_type"] = each["leaf"]["system_type"]
+                if each["leaf"]["label"] == "leaf2":
+                    leafs["leaf2"] = {}
+                    leafs["leaf2"]["hostname"] = each["leaf"]["hostname"]
+                    leafs["leaf2"]["id"] = each["leaf"]["id"]
+                    leafs["leaf2"]["system_id"] = each["leaf"]["system_id"]
+                    leafs["leaf2"]["system_type"] = each["leaf"]["system_type"]
+                if each["leaf"]["label"] == "leaf3":
+                    leafs["leaf3"] = {}
+                    leafs["leaf3"]["hostname"] = each["leaf"]["hostname"]
+                    leafs["leaf3"]["id"] = each["leaf"]["id"]
+                    leafs["leaf3"]["system_id"] = each["leaf"]["system_id"]
+                    leafs["leaf3"]["system_type"] = each["leaf"]["system_type"]
+
+        return leafs
