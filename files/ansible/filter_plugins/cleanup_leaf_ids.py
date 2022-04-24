2832fc7f65b9e497d611c6774987dfb226bc378f
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-04-24 11:57:42 +0000

    test dict

--- files/ansible/filter_plugins/cleanup_leaf_ids.py
+++ files/ansible/filter_plugins/cleanup_leaf_ids.py
@@ -13,23 +13,23 @@ class FilterModule(object):
     def cleanup_leaf_ids(self, leaf_system_ids):
         """Filter out filler from the response of our blueprint health."""
         leafs = {}
+        leafs["leaf1"] = {}
+        leafs["leaf2"] = {}
+        leafs["leaf3"] = {}
 
         if isinstance(leaf_system_ids, list):
             for each in leaf_system_ids:
                 if each["leaf"]["label"] == "leaf1":
-                    leafs["leaf1"] = {}
                     leafs["leaf1"]["hostname"] = each["leaf"]["hostname"]
                     leafs["leaf1"]["id"] = each["leaf"]["id"]
                     leafs["leaf1"]["system_id"] = each["leaf"]["system_id"]
                     leafs["leaf1"]["system_type"] = each["leaf"]["system_type"]
                 if each["leaf"]["label"] == "leaf2":
-                    leafs["leaf2"] = {}
                     leafs["leaf2"]["hostname"] = each["leaf"]["hostname"]
                     leafs["leaf2"]["id"] = each["leaf"]["id"]
                     leafs["leaf2"]["system_id"] = each["leaf"]["system_id"]
                     leafs["leaf2"]["system_type"] = each["leaf"]["system_type"]
                 if each["leaf"]["label"] == "leaf3":
-                    leafs["leaf3"] = {}
                     leafs["leaf3"]["hostname"] = each["leaf"]["hostname"]
                     leafs["leaf3"]["id"] = each["leaf"]["id"]
                     leafs["leaf3"]["system_id"] = each["leaf"]["system_id"]
